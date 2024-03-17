9d3c627453247118efbce65d31dbcbb67d2fd3d9
Luca <luca@ahd-creative.agency>
2024-03-17 18:03:46 +0100

    feat(elements); button

---
+++ src/components/utils/Button.tsx
@@ -0,0 +1,14 @@
+import {ReactNode} from "react";
+
+type ButtonProps = {
+    className?: string,
+    children: ReactNode,
+    onClick?: any
+}
+
+export const Button = (props: ButtonProps) => {
+    <button onClick={props.onClick} type="button"
+            className={`rounded-full py-3 font-bold shadow-md-dark-contrast transition focus:outline-none ${props.className}`}>
+        {props.children}
+    </button>
+};
